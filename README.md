14942e185f17265ad0cf77ed0dc9531fcb980629
jacekv <jaca347@gmail.com>
2021-03-25 15:11:56 +0100

    Added download functionality

--- README.md
+++ README.md
@@ -1,30 +1,35 @@
-# Upload ABI To Pinata
+# PinABI
 
 During the development of Smart Contracts using Hardhat I started to think on how to share the Contract's ABI with the backend and frontend part. Copy and paste seemed stupido.
-So, `Upload ABI to Pinata` was born.
+So, `PinABI` was born. With `PinABI` you are able to up- and download your smart contracts ABI to/from IPFS and pin with Pinata.
 
 ## Create Api Key at Pinata
-In order to use it, you have to create a Pinata account. Go to [Pinata](https://pinata.cloud) and create an account. Once this is done, you will have to create an API key.
-You will require the following permissions:
+In order to use it, you have to create a Pinata account. Go to [Pinata](https://pinata.cloud) and create an account. 
+Once this is done, you should create ideally two API keys - one for uploading an ABI and one for downloading.
+Your uploading API Key will require the following permissions:
 
     * pinJSONToIPFS
     * unpin
     * pinList
 
+Your downloading API Key will require the following permissions:
+
+    * pinList
+
 Copy the api key and secret. You will have to export those values in your Terminal:
-````
+````bash
 export PINATA_API_KEY="Key Value" 
 export PINATA_API_SECRET="Secret Value"
 ````
 and you are good to go :)
 
 ## Use in Hardhat
-`Upload ABI to Pinata` is currently written for [Hardhat](https://https://hardhat.org/) to be used as a task.
+`PinABI` is currently written for [Hardhat](https://https://hardhat.org/) to be used as a task.
 
-In order to use it in Hardhat, go tyou your `.config.js`file and add it as a task:
+In order to use it in Hardhat, go to your `.config.js`file and add it as a task:
 
-```
-const { uploadABIToPinata } = require("upload-abi-to-pinata");
+```javascript
+const { uploadABIToPinata } = require("pin-abi");
 
 task("upload", "Upload Smart Contracts ABI to Pinata")
   .addParam("name", "The name for the Pinata Pin")
@@ -40,12 +45,21 @@ npx hardhat upload --name [some pinning name]
 
 ## Which Smart Contracts ABIs to upload?
 
-In order to pick which ABIs you want to upload, create a `.uploadABI.json` file in your root folder of your hardhat project:
-```
+In order to pick which ABIs you want to upload, create a `.uploadABI.json` file in your root folder of your Hardhat project:
+```json
 {
     "files": [
         "Greeter.sol" # this represents the artifacts/contracts/Greeter.sol/ directory structure
     ]
 }
+```
+
+## Download ABIs
+If you want to download your Smart Contracts ABIs in a different project, you can do the following:
+```javascript
+const { downloadABIFromPinata } = require("pin-abi");
+
+const pinName = "A name you used during the upload"
+await downloadABIFromPinata(pinName);
 ```
 Enjoy and I hope it is usefull :)
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 const { exit } = require("process");
 const pinataSDK = require('@pinata/sdk');
+const axios = require('axios');
 
 const HARDHAT_CONTRACT_PATH='./artifacts/contracts/';
-
+const URL = 'https://gateway.pinata.cloud/ipfs/';
 let pinata;
 
 /**
@@ -20,6 +21,7 @@ function readFile(path) {
  * @returns Pianta object
  */
 function loadPinata() {
+    console.log("Loading Pinata client");
     if (!process.env.PINATA_API_KEY || !process.env.PINATA_API_SECRET) {
         console.log("PINATA_API_KEY or PINATA_API_SECRET is not set");
         exit(1);
@@ -32,7 +34,8 @@ function loadPinata() {
  * @param {*} name - Name to filter pinned objects on Pinata
  * @returns An object of pinned object using the given name
  */
-async function getPinList(name) {
+function getPinList(name) {
+    console.log('Getting the pin list...');
     const filter = {
         status: 'pinned',
         metadata: {
@@ -64,18 +67,31 @@ async function unpin(name) {
 }
 
 /**
+ * The function checks if the artifacts/contracts directiry exist. If not, they
+ * will be created.
+ */
+function createABIDirectory(dirPath) {
+    if (!fs.existsSync(dirPath)) {
+        fs.mkdirSync(dirPath, { recursive: true });
+    }
+}
+
+function writeFile(filePath, content) {
+    fs.writeFileSync(filePath, content);
+}
+
+/**
  * Function which does all the work. Checks a pin on Pinata exists, and unpins it.
  * Than takes the abi of smart contracts and uploads those to Pinata.
  * @param {*} name - Name to be given to the pinned object
  * @returns Object with ipds hash
  */
-async function uploadABIToPinata(name) {
+function uploadABIToPinata(name) {
     if (name === "" || name === undefined) {
         console.log("Parameter name is not set");
         exit(1);
     }
     // credentials are stored in PINATA_API_KEY and PINATA_API_SECRET
-    console.log("Loading Pinata client");
     pinata = loadPinata();
     console.log(`Cheking if pin with name ${name} exists`);
     if (await checkIfPinExists(name)) {
@@ -87,17 +103,34 @@ async function uploadABIToPinata(name) {
         contracts: {}
     }
     let file;
+    let filePath;
+    let extension;
     let content;
     //Currently only for .sol files. The directory has .sol in it
     uploadContracts.files.forEach((contract) => {
+        if (contract.includes('.sol')) {
+            extension = '.sol';
+        } else {
+            extension = '';
+        }
+
         if (contract.includes('/')) {
+            filePath = contract.split('/');
+            filePath.pop();
+            filePath = filePath.join('/');
             file = `${HARDHAT_CONTRACT_PATH}${contract}/${contract.split('/')[1].split('.')[0]}.json`;
         } else {
+            filePath = '.';
             file = `${HARDHAT_CONTRACT_PATH}${contract}/${contract.split('.')[0]}.json`;
         }
+
         if (fs.existsSync(file)) {
             content = JSON.parse(readFile(file));
-            pinataBody.contracts[content.contractName] = content.abi;
+            pinataBody.contracts[`${content.contractName}`] = {
+                abi: content.abi,
+                path: filePath,
+                extension
+            };
         } else {
             console.log(`${file} doesn't exist`);
             exit(1);
@@ -115,6 +148,51 @@ async function uploadABIToPinata(name) {
     return pinata.pinJSONToIPFS(pinataBody, options);
 }
 
+/**
+ * downloadABIFromPinata is used in order to download Smart Contracts ABI's from IPFS
+ * using Pinata's Pinning name. It's going to save those ABIs in the artifacts/contracts
+ * directory.
+ * @param {*} name - Pinning name used during the upload of the ABIs 
+ */
+function downloadABIFromPinata(name) {
+    if (name === "" || name === undefined) {
+        console.log("Parameter name is not set");
+        exit(1);
+    }
+    createABIDirectory(HARDHAT_CONTRACT_PATH);
+    pinata = loadPinata();
+    const pinList = await getPinList(name);
+    if (pinList.count == 0) {
+        console.log(`Pin with name "${name}" doesn't exist`);
+        exit(1);
+    }
+    if (pinList.count > 1) {
+        console.log(`There are multiple pins with the same name. How?`);
+    }
+
+    const params = {
+        url: `${URL}${pinList.rows[0]['ipfs_pin_hash']}`,
+        method: 'get'
+    }
+    const result = await axios(params);
+    const contracts = result.data.contracts;
+    const contractNames = Object.keys(contracts);
+    let dirPath;
+    let pathToWrite;
+    for (let contractName of contractNames) {
+        dirPath = `${HARDHAT_CONTRACT_PATH}${contracts[contractName].path}/${contractName}${contracts[contractName].extension}`;
+        createABIDirectory(dirPath);
+        pathToWrite = `${dirPath}/${contractName}.json`;
+        writeFile(pathToWrite, JSON.stringify({
+            contractName: contractName,
+            sourceName: dirPath.replace('./artifacts/', ''),
+            abi: contracts[contractName].abi
+        }, null, 2));
+    }
+    console.log('Wrote abi to files');
+}
+
 module.exports = {
-    uploadABIToPinata
+    uploadABIToPinata,
+    downloadABIFromPinata
 }
